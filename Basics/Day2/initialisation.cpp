91054529d56e4ddc8bb95146c7e95316a1aa2bc9
Aadhil Kassim <aadhikassim@gmail.com>
2024-08-09 16:26:07 +0530

    Initialisation
    Tried out different ways

---
+++ Basics/Day2/initialisation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{   //Braced Initalisation
+    int a {15}; //Decimal
+
+    //Functional Initialisation
+    int b (017); // Octal
+
+    //Assignment initialisation
+    int c = 0x0f; // Hexadecimal
+
+
+    cout << "Number: " << a << endl; //Outputs 15
+    cout << "Number: " << b << endl; //Outputs 15
+    cout << "Number: " << c << endl; //Outputs 15
+    return 0;
+}
