3279ce0bbeec821d06bed427774a26fd0e94abe9
Aadhil Kassim <aadhikassim@gmail.com>
2024-08-10 12:43:23 +0530

    Up

--- Basics/Day2/float.cpp
+++ Basics/Day2/float.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 { 
-   float a {1.945678652135487786512f}; //Presicion 7
-   double b {1.48756545651211545622}; //Presicion 12
-  long double c {1.45223878897464324544488544L}; //Presicion 16
+   float a {1.945678652135487786512f}; //Precision 7
+   double b {1.48756545651211545622}; //Precision 12
+  long double c {1.45223878897464324544488544L}; //Precision 16
 
   cout << setprecision(20);
   cout << a << endl <<b << endl << c <<endl;
