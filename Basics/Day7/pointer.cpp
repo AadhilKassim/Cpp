eeeae798d76a5d68a78e2b04616918d015074ea3
Aadhil Kassim <aadhikassim@gmail.com>
2024-08-14 15:56:51 +0530

    Pointer

--- Basics/Day7/pointer.cpp
+++ Basics/Day7/pointer.cpp
@@ -9,7 +9,7 @@ int main()
     cout << p << endl; //
     cout << p1 <<endl;
     cout << "Size of (int): " << sizeof(int*) << endl;
-    cout << "Size of (p): " << sizeof(p) << endl;
-    
+    cout << "Size of (p): " << sizeof(p) << endl; //8 bytes
+    cout << "Size of (p1): " << sizeof(p1) << endl; //8 bytes
     return 0;
 }
