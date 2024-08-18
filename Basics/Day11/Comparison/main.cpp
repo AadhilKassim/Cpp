bfb604fffe76ae518252f14954cd288431d9bd28
Aadhil Kassim <aadhikassim@gmail.com>
2024-08-18 16:28:13 +0530

    Header usage

---
+++ Basics/Day11/Comparison/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "compare.h"
+//using namespace std;
+int main()
+{
+    int Max=max(123,89);
+    std::cout << "Max: " << Max << std::endl;
+    int Min=min(134,78);
+    std::cout << "Min: " << Min << std::endl;
+}
